853bbb55b20105a5d164b6f14ba167e819068dd5
ravista <rsubedi@altimetrik.com>
2017-04-21 01:03:57 -0400

    color change

--- myRandomFacts/colorsSource.swift
+++ myRandomFacts/colorsSource.swift
@@ -10,7 +10,7 @@ import UIKit
 import GameKit
 struct Colors{
     let colorList = [
-    UIColor(colorLiteralRed: 192.00, green: 0.00, blue: 192.00, alpha: 1.0),
+    UIColor(colorLiteralRed: 192.00, green: 0.00, blue: 255.00, alpha: 1.0),
     UIColor(colorLiteralRed: 128.00, green: 128.00, blue: 0.00, alpha: 1.0),
     UIColor(colorLiteralRed: 100.0, green: 0.00, blue: 250.00, alpha: 1.0),
     UIColor(colorLiteralRed: 255.00, green: 0.00, blue: 0.00, alpha: 1.0),
