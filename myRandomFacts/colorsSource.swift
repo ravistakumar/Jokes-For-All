bf602c7ce16cb13872a776a7ff341cb7d1af4ee9
ravista <rsubedi@altimetrik.com>
2017-04-21 02:07:22 -0400

    small tweaks

--- myRandomFacts/colorsSource.swift
+++ myRandomFacts/colorsSource.swift
@@ -10,21 +10,15 @@ import UIKit
 import GameKit
 struct Colors{
     let colorList = [
-    UIColor(colorLiteralRed: 192.00, green: 0.00, blue: 255.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 128.00, green: 128.00, blue: 0.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 100.0, green: 0.00, blue: 250.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 255.00, green: 0.00, blue: 0.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 128.00, green: 0.00, blue: 50.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 255.00, green: 255.00, blue: 0.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 128.00, green: 128.00, blue: 0.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 255.00, blue: 255.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 128.00, blue: 0.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 255.00, blue: 255.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 128.00, blue: 128.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 0.00, blue: 255.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 0.00, green: 0.00, blue: 128.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 255.00, green: 0.00, blue: 255.00, alpha: 1.0),
-    UIColor(colorLiteralRed: 128.00, green: 0.00, blue: 128.00, alpha: 1.0)]
+    UIColor(colorLiteralRed: 100/255, green: 30/255, blue: 22/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 74/255, green: 35/255, blue: 90/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 21/255, green: 67/255, blue: 96/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 27/255, green: 79/255, blue: 114/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 11/255, green: 83/255, blue: 69/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 20/255, green: 90/255, blue: 50/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 24/255, green: 106/255, blue: 59/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 46/255, green: 64/255, blue: 83/255, alpha: 1.0),
+    UIColor(colorLiteralRed: 128/255, green: 139/255, blue: 150/255, alpha: 1.0)]
     func giveMeSomeColors()-> UIColor{
     return colorList[GKRandomSource.sharedRandom().nextInt(upperBound: colorList.count)]
     }
